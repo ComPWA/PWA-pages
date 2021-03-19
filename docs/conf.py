3b68df5591f5be56f4cb0381bc6a07bccb575db8
Remco de Boer <redeboer@gmx.com>
2021-03-19 14:21:31 +0100

    docs: add activate Sphinx graphviz extension

--- docs/conf.py
+++ docs/conf.py
@@ -64,6 +64,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosectionlabel",
     "sphinx.ext.doctest",
+    "sphinx.ext.graphviz",
     "sphinx.ext.intersphinx",
     "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
@@ -96,6 +97,7 @@ autodoc_default_options = {
         ]
     ),
 }
+graphviz_output_format = "svg"
 html_copy_source = True  # needed for download notebook button
 html_favicon = "_static/favicon.ico"
 html_show_copyright = False
