ffafb7d840b3ccccfaf6952bab52deaee5fa3ae5
Remco de Boer <redeboer@gmx.com>
2021-04-10 21:12:18 +0200

    fix: enforce type hints docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,3 @@
-# type: ignore
-
 """Configuration file for the Sphinx documentation builder.
 
 This file only contains a selection of the most common options. For a full
@@ -10,11 +8,9 @@ https://www.sphinx-doc.org/en/master/usage/configuration.html
 import os
 import shutil
 import subprocess
-from typing import Dict
 
 from docutils import nodes
 from pkg_resources import get_distribution
-from sphinx.application import Sphinx
 
 # -- Project information -----------------------------------------------------
 project = "PWA Software Pages"
@@ -208,10 +204,18 @@ thebe_config = {
 }
 
 # Add roles to simplify external links
+from typing import Any, Dict, List, Optional, Tuple
+
+from docutils.nodes import Node as docutils_Node
+from docutils.nodes import system_message
+from docutils.parsers.rst.states import Inliner
+from sphinx.application import Sphinx
+from sphinx.util.typing import RoleFunction
+
 pdg_version = 2020
 
 
-def setup(app: Sphinx):
+def setup(app: Sphinx) -> Dict[str, Any]:
     app.add_role(
         "wiki",
         wikilink("https://en.wikipedia.org/wiki/%s"),
@@ -228,31 +232,51 @@ def setup(app: Sphinx):
             f"https://pdg.lbl.gov/{pdg_version}/reviews/rpp{pdg_version}-rev-%s.pdf"
         ),
     )
+    return {
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
+    }
 
 
-def pdglink(pattern: str):
+def pdglink(pattern: str) -> RoleFunction:
     def role(
-        name, rawtext: str, text: str, lineno, inliner, options={}, content=[]
-    ):
+        name: str,
+        rawtext: str,
+        text: str,
+        lineno: int,
+        inliner: Inliner,
+        options: Optional[Dict] = None,
+        content: Optional[List[str]] = None,
+    ) -> Tuple[List[docutils_Node], List[system_message]]:
         label = text
         label = f"§ {label}"
         url = text
         url = url.lower()
         url = url.replace(" ", "-")
         url = pattern % (url,)
+        if options is None:
+            options = dict()
         node = nodes.reference(rawtext, label, refuri=url, **options)
         return [node], []
 
     return role
 
 
-def wikilink(pattern: str):
+def wikilink(pattern: str) -> RoleFunction:
     def role(
-        name, rawtext: str, text: str, lineno, inliner, options={}, content=[]
-    ):
+        name: str,
+        rawtext: str,
+        text: str,
+        lineno: int,
+        inliner: Inliner,
+        options: Optional[Dict] = None,
+        content: Optional[List[str]] = None,
+    ) -> Tuple[List[docutils_Node], List[system_message]]:
         output_text = text
         output_text = output_text.replace("_", " ")
         url = pattern % (text,)
+        if options is None:
+            options = dict()
         node = nodes.reference(rawtext, output_text, refuri=url, **options)
         return [node], []
 
@@ -263,10 +287,10 @@ def wikilink(pattern: str):
 import dataclasses
 from typing import Union
 
-import sphinxcontrib.bibtex.plugin
+import sphinxcontrib.bibtex.plugin  # type: ignore
 from pybtex.database import Entry
 from pybtex.plugin import register_plugin
-from pybtex.richtext import Tag, Text
+from pybtex.richtext import BaseText, Tag, Text
 from pybtex.style.formatting.unsrt import Style as UnsrtStyle
 from pybtex.style.template import (
     FieldIsMissing,
@@ -279,7 +303,7 @@ from pybtex.style.template import (
     sentence,
     words,
 )
-from sphinxcontrib.bibtex.style.referencing.author_year import (
+from sphinxcontrib.bibtex.style.referencing.author_year import (  # type: ignore
     AuthorYearReferenceStyle,
 )
 
@@ -297,7 +321,7 @@ sphinxcontrib.bibtex.plugin.register_plugin(
 
 
 @node
-def et_al(children, data, sep="", sep2=None, last_sep=None):
+def et_al(children, data, sep="", sep2=None, last_sep=None):  # type: ignore
     if sep2 is None:
         sep2 = sep
     if last_sep is None:
@@ -314,7 +338,7 @@ def et_al(children, data, sep="", sep2=None, last_sep=None):
 
 
 @node
-def names(children, context, role, **kwargs):
+def names(children, context, role, **kwargs):  # type: ignore
     """Return formatted names."""
     assert not children
     try:
@@ -330,10 +354,10 @@ def names(children, context, role, **kwargs):
 
 
 class MyStyle(UnsrtStyle):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__(abbreviate_names=True)
 
-    def format_names(self, role, as_sentence=True) -> Node:
+    def format_names(self, role, as_sentence: bool = True) -> Node:  # type: ignore
         formatted_names = names(
             role, sep=", ", sep2=" and ", last_sep=", and "
         )
@@ -342,7 +366,7 @@ class MyStyle(UnsrtStyle):
         else:
             return formatted_names
 
-    def format_eprint(self, e):
+    def format_eprint(self, e: Entry) -> Node:
         if "doi" in e.fields:
             return ""
         return super().format_eprint(e)
