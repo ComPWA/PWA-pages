3b77f32d9803cd35fb5a540990df1a050177a52f
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2023-02-19 17:31:24 +0100

    TEMP: debug attempt

--- docs/conf.py
+++ docs/conf.py
@@ -59,6 +59,7 @@ def get_branch_name() -> str:
         branch = "main"
     if branch is None:
         branch = os.environ.get("GITHUB_REF", "main")
+        raise ValueError(f'"{branch}"')
         if re.match(r"^\d+/[a-z]+$", branch) is not None:  # type: ignore[arg-type]
             branch = "main"  # PR preview
         else:
