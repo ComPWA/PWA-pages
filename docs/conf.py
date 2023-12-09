2633a1568daa4985cdd1c677d0398559889cc2fa
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2023-12-09 17:17:20 +0100

    DX: outsource bibliography style to `sphinx-pybtex-etal-style`

--- docs/conf.py
+++ docs/conf.py
@@ -16,26 +16,11 @@ from functools import lru_cache
 from typing import TYPE_CHECKING, Any
 
 from docutils import nodes
-from pybtex.plugin import register_plugin
-from pybtex.richtext import Tag, Text
-from pybtex.style.formatting.unsrt import Style as UnsrtStyle
-from pybtex.style.template import (
-    FieldIsMissing,
-    Node,
-    _format_list,
-    field,
-    href,
-    join,
-    node,
-    sentence,
-    words,
-)
 
 if TYPE_CHECKING:
     from docutils.nodes import Node as docutils_Node
     from docutils.nodes import system_message
     from docutils.parsers.rst.states import Inliner
-    from pybtex.database import Entry
     from sphinx.application import Sphinx
     from sphinx.util.typing import RoleFunction
 
@@ -113,6 +98,7 @@ extensions = [
     "sphinx_copybutton",
     "sphinx_design",
     "sphinx_hep_pdgref",
+    "sphinx_pybtex_etal_style",
     "sphinx_thebe",
     "sphinx_togglebutton",
     "sphinxcontrib.bibtex",
@@ -346,90 +332,3 @@ def wikilink(pattern: str) -> RoleFunction:
         return [reference_node], []
 
     return role
-
-
-@node
-def et_al(children, data, sep="", sep2=None, last_sep=None):  # type: ignore[no-untyped-def]
-    if sep2 is None:
-        sep2 = sep
-    if last_sep is None:
-        last_sep = sep
-    parts = [part for part in _format_list(children, data) if part]
-    if len(parts) <= 1:
-        return Text(*parts)
-    if len(parts) == 2:  # noqa: PLR2004
-        return Text(sep2).join(parts)
-    if len(parts) == 3:  # noqa: PLR2004
-        return Text(last_sep).join([Text(sep).join(parts[:-1]), parts[-1]])
-    return Text(parts[0], Tag("em", " et al"))
-
-
-@node
-def names(children, context, role, **kwargs):  # type: ignore[no-untyped-def]
-    """Return formatted names."""
-    assert not children
-    try:
-        persons = context["entry"].persons[role]
-    except KeyError:
-        raise FieldIsMissing(role, context["entry"]) from None
-
-    style = context["style"]
-    formatted_names = [
-        style.format_name(person, style.abbreviate_names) for person in persons
-    ]
-    return et_al(**kwargs)[formatted_names].format_data(context)
-
-
-class MyStyle(UnsrtStyle):  # pyright: ignore[reportUntypedBaseClass]
-    def __init__(self) -> None:
-        super().__init__(abbreviate_names=True)
-
-    def format_names(self, role, as_sentence: bool = True) -> Node:  # type: ignore[no-untyped-def]
-        formatted_names = names(role, sep=", ", sep2=" and ", last_sep=", and ")
-        if as_sentence:
-            return sentence[formatted_names]
-        return formatted_names
-
-    def format_eprint(self, e: Entry) -> Node:
-        if "doi" in e.fields:
-            return ""
-        return super().format_eprint(e)
-
-    def format_url(self, e: Entry) -> Node:
-        if "doi" in e.fields or "eprint" in e.fields:
-            return ""
-        return words[
-            href[
-                field("url", raw=True),
-                field("url", raw=True, apply_func=remove_http),
-            ]
-        ]
-
-    def format_isbn(self, e: Entry) -> Node:
-        return href[
-            join[
-                "https://isbnsearch.org/isbn/",
-                field("isbn", raw=True, apply_func=remove_dashes_and_spaces),
-            ],
-            join[
-                "ISBN:",
-                field("isbn", raw=True),
-            ],
-        ]
-
-
-def remove_dashes_and_spaces(isbn: str) -> str:
-    to_remove = ["-", " "]
-    for remove in to_remove:
-        isbn = isbn.replace(remove, "")
-    return isbn
-
-
-def remove_http(url: str) -> str:
-    to_remove = ["https://", "http://"]
-    for remove in to_remove:
-        url = url.replace(remove, "")
-    return url
-
-
-register_plugin("pybtex.style.formatting", "unsrt_et_al", MyStyle)
